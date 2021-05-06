bff3ff6fc04ca45b756e404fd5d6acf73e173a29
김관희 <rhks122314@gmail.com>
2021-05-06 21:09:20 +0900

    리뷰 1차

--- src/Components/Story/Story.js
+++ src/Components/Story/Story.js
@@ -1,15 +1,20 @@
-import { useState } from "react";
-import RoundProfile from "../RoundProfile/RoundProfile";
-import "./Story.scss";
+import { useState } from 'react';
+import RoundProfile from '../RoundProfile/RoundProfile';
+import './Story.scss';
+
+// 6과 같은 숫자가 반복되서 한 번만 변수로 선언해주고 사용하는 것이 더 좋습니다
+// const STORY_LIMIT = 6;
 
 function Story(props) {
   const [storyStart, setStart] = useState(0);
 
   const goPrev = () => {
     setStart(storyStart - 6);
+    // setStart(storyStart - STORY_LIMIT);
   };
   const goNext = () => {
     setStart(storyStart + 6);
+    // setStart(storyStart + STORY_LIMIT);
   };
 
   return (
@@ -19,19 +24,21 @@ function Story(props) {
         onClick={goPrev}
         style={
           storyStart === 0
-            ? { visibility: "hidden" }
-            : { visibility: "visible" }
+            ? { visibility: 'hidden' }
+            : { visibility: 'visible' }
+          // 리뷰: {visibility: storyStart === 0 ? "hidden" : "visible"}
         }
       >
-        {"<"}
+        {'<'}
       </button>
+      {/* 여기서도 STORY_LIMIT 을 쓸 수 있겠네요! */}
       {props.storyData.slice(storyStart, storyStart + 6).map((user, index) => {
         return (
           <div className="storyUser" key={index}>
             <RoundProfile
               username={user.username}
               profileImg={user.profileImg}
-              size={"80px"}
+              size={'80px'}
             />
             <span className="username">{user.username}</span>
           </div>
@@ -41,12 +48,13 @@ function Story(props) {
         className="goNext"
         onClick={goNext}
         style={
+          // 여기서도 STORY_LIMIT 을 쓸 수 있겠네요!
           storyStart + 6 >= props.storyData.length
-            ? { visibility: "hidden" }
-            : { visibility: "visible" }
+            ? { visibility: 'hidden' }
+            : { visibility: 'visible' }
         }
       >
-        {">"}
+        {'>'}
       </button>
     </section>
   );
--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,33 +1,40 @@
-import { useState } from "react";
-import Icon from "../../Components/Icon/Icon";
-import ProfileBox from "../../Components/ProfileBox/ProfileBox";
-import Story from "../../Components/Story/Story";
-import Post from "../../Components/Post/Post";
-import profileData from "../../data/profileData";
-import storyData from "../../data/storyData";
-import "./Profile.scss";
+import { useState } from 'react';
+import Icon from '../../Components/Icon/Icon';
+import ProfileBox from '../../Components/ProfileBox/ProfileBox';
+import Story from '../../Components/Story/Story';
+import Post from '../../Components/Post/Post';
+import profileData from '../../data/profileData';
+import storyData from '../../data/storyData';
+import './Profile.scss';
 
 function Profile() {
-  const [menu, setMenu] = useState("Posts");
+  const [menu, setMenu] = useState('Posts');
 
-  const clickMenu = (selectedMenu) => {
+  const clickMenu = selectedMenu => {
     setMenu(selectedMenu);
   };
   const data = profileData[1];
+  // 리뷰: 굳이 data 변수에 담을 이유가 없을 것 같아요!
+
+  // const menuObj = {
+  //   "Posts": <Post />,
+  //   "Tagged": null
+  // }
 
   return (
     <section className="Profile">
       <ProfileBox data={data} />
       <Story storyData={storyData} />
+
       <nav className="profileNav">
         <ul>
           <li
             className="Posts"
-            onClick={() => clickMenu("Posts")}
+            onClick={() => clickMenu('Posts')}
             style={
-              menu === "Posts"
-                ? { color: "black", borderTopColor: "black" }
-                : { color: "#d3d3d3", borderTopColor: "#d3d3d3" }
+              menu === 'Posts'
+                ? { color: 'black', borderTopColor: 'black' }
+                : { color: '#d3d3d3', borderTopColor: '#d3d3d3' }
             }
           >
             <Icon className="fas fa-table" />
@@ -35,20 +42,25 @@ function Profile() {
           </li>
           <li
             className="Tagged"
-            onClick={() => clickMenu("Tagged")}
+            onClick={() => clickMenu('Tagged')}
             style={
-              menu === "Tagged"
-                ? { color: "black", borderTopColor: "black" }
-                : { color: "#d3d3d3", borderTopColor: "#d3d3d3" }
+              menu === 'Tagged'
+                ? { color: 'black', borderTopColor: 'black' }
+                : { color: '#d3d3d3', borderTopColor: '#d3d3d3' }
             }
           >
             <Icon className="fas fa-id-card-alt" />
             Tagged
           </li>
+          {/* 리뷰: nav부분도 같이 컴포넌트로 빼주시는 게 더 가독성이 좋을 것 같아요! */}
+          {/* li 태그 내에서 Posts, Tagged 빼고는 다 동일해서
+          Array.map()을 사용하면 반복을 줄일 수 있을 것 같아요! */}
         </ul>
       </nav>
-      {menu === "Posts" && <Post data={data.postImg} />}
-      {menu === "Tagged" && null}
+      {menu === 'Posts' && <Post data={data.postImg} />}
+      {menu === 'Tagged' && null}
+      {/* {menuObj[menu]} */}
+      {/* 리뷰: 이렇게 메뉴에 들어가는 컴포넌트를 객체에 저장해서 사용할 수 있습니다. */}
     </section>
   );
 }
